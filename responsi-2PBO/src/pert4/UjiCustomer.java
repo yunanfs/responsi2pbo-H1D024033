f28da476893faedf2b95e4a7eb7f60ad76c7ef15
YUNAN FAILA SOFI <yunanpkl1@gmail.com>
2025-12-07 11:33:33 +0700

    Update customer and member objects in UjiCustomer

--- responsi-2PBO/src/pert4/UjiCustomer.java
+++ responsi-2PBO/src/pert4/UjiCustomer.java
@@ -3,22 +3,22 @@ package pert4;
 public class UjiCustomer {
     public static void main(String[] args) {
 
-        // 1. Buat object Customer (Budi)
-        Customer customerBudi = new Customer("Budi Santoso", "CST-001", 500000);
+        // 1. Buat object Customer (Yunan)
+        Customer customerYunan = new Customer("Yunan Unsoed", "CST-001", 500000);
 
-        // 2. Buat object Member (Siti)
-        Member memberSiti = new Member("Siti Aminah", "MBR-110", 1250000, 240, "Gold");
+        // 2. Buat object Member (Faila)
+        Member memberFaila = new Member("Faila Unsoed", "MBR-110", 1250000, 240, "Gold");
 
         // Tampilkan judul data pelanggan
         System.out.println("=== DATA CUSTOMER BELANJAKU ===");
 
         // --- Customer Biasa ---
         System.out.println("Status: Customer Biasa");
-        customerBudi.tampilkanInfo();
-        System.out.println(); // Jarak baris
+        customerYunan.tampilkanInfo();
+        System.out.println(); 
 
         // --- Pelanggan Member ---
         System.out.println("Status: Member");
-        memberSiti.tampilkanInfo();
+        memberFaila.tampilkanInfo();
     }
 }
